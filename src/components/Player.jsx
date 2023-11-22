b96da5352bc7f979806d8f23f351fdf00f0da733
CodeSandbox <support@codesandbox.io>
2023-11-22 16:11:22 +0000

    Sandbox snapshot commit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,17 @@
+import React, { useState, useRef } from "react";
+
 export default function Player() {
+  const playerRef = useRef();
+  const [playerName, setPlayerName] = useState("");
+  function onClickHandler() {
+    setPlayerName(playerRef.current.value);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ? playerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerRef} type="text" />
+        <button onClick={onClickHandler}>Set Name</button>
       </p>
     </section>
   );
